502474a65eb402bb55d6f85dbfcaeffbca87711b
travis brimhall <travisb@opentable.com>
2022-03-04 10:24:33 -0800

    add instructions

---
+++ src/components/services/Availability.js
@@ -0,0 +1,15 @@
+// READ: Input data is in the test file
+
+export const generateTimeSlots = shift => {
+  // TODO: fill in this function to generate timeslots for the shift
+  const timeslots = [];
+  return timeslots;
+};
+    
+export const calculateAvailability = (timeslots, reservations) => {
+  /*
+    TODO: Calculate the availability of the shift times using the reservations.
+    We want to know for each time if there is a table available.
+  */
+  return timeslots;
+};
