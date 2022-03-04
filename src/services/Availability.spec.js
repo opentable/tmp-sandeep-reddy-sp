d6507132a47b5336533e3d591f81621505e4b2be
Agustin Colchado <agustin@colchado.com>
2022-03-04 10:42:32 -0800

    Update Availability.spec.js

--- src/services/Availability.spec.js
+++ src/services/Availability.spec.js
@@ -1,6 +1,13 @@
 import { calculateAvailability, generateTimeSlots } from "./Availability";
 
-// INTERVIEWER to provive configuration for the input: https://github.com/opentable/availabilityChallenge_interviewRepo/wiki
+// All values are in minutes ( 10 * 60 = 600 or 10am )
+const shift = { start: 600, end: 900, interval: 15, tables: 2 };
+
+const reservations = [
+  { scheduled: 600, duration: 90 },
+  { scheduled: 675, duration: 75 },
+  { scheduled: 690, duration: 90 }
+];
 
 describe("generateTimeSlots()", () => {
   it("should return an empty array if there is no shift argument", () => {
